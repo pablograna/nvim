fa73b748d77948dde08764d5e11bc7079af2edf4
Pablo Gra\~na <pablo.grana@mercadolibre.com>
2024-10-30 12:27:40 -0300

    Set scroll offset to 0, nvim-qt config.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show line numbers starting at 1.
 vim.opt.relativenumber = false
+
+-- Do not h-scroll when getting close to the right margin.
+vim.opt.sidescrolloff = 0
+
+-- Do not v-scroll when getting close to the bottom of the window.
 vim.opt.scrolloff = 0
+
+-- Place a vertical marker after column 80.
+vim.opt.colorcolumn = "81"
